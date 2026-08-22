ff0ef35882f96fdc2db8fc917fd4be26b1d81142
Vladislav <vladpivovarov2797@ya.ru>
2026-08-22 17:54:54 +0300

    Добавляет функцию проверки времени встречи

--- js/functions.js
+++ js/functions.js
@@ -31,3 +31,22 @@ const retrieve = (line) => {
 };
 
 retrieve('1 кефир, 0.5 батона');
+
+
+// Функция проверяет что встреча не выходит за рамки рабочего дня
+const isWorkdayValid = (start, end, meetStart, meetDuration) => {
+  const getMinutes = (time) => {
+    const [hours, minutes] = time.split(':');
+    return hours * 60 + +minutes;
+  };
+
+  const startTimeMinutes = getMinutes(start);
+  const endTimeMinutes = getMinutes(end);
+  const meetStartTimeMinutes = getMinutes(meetStart);
+  const endMeet = meetStartTimeMinutes + meetDuration;
+
+  // Встреча должна начинаться не раньше начала дня и заканчиваться не позже окончания дня
+  return meetStartTimeMinutes >= startTimeMinutes && endMeet <= endTimeMinutes;
+};
+
+isWorkdayValid('08:00', '17:30', '17:00', 30);
