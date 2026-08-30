05d9835282760fb0c9a5a243c780d5581ace7579
Vladislav <vladpivovarov2797@ya.ru>
2026-08-30 20:33:25 +0300

    rename gallery function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { generatePhotoData } from './generate-photo-data.js';
 import { printThumbnails, thumbnailsBundle } from './thumbnails-maker.js';
-import { initGalleryListeners } from './gallery-handler.js';
+import { initGallery } from './gallery-handler.js';
 
 const thumbnailsData = generatePhotoData();
 
 printThumbnails(thumbnailsData);
-initGalleryListeners(thumbnailsBundle);
+initGallery(thumbnailsBundle);
 
