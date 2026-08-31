97b81ba2b864c5665353dc715f0e4c8c66475ba8
Keks <keks@htmlacademy.ru>
2026-08-31 19:00:45 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,9 +1,9 @@
 import { generatePhotoData } from './generate-photo-data.js';
-import { printThumbnails, thumbnailsBundle } from './thumbnails-maker.js';
-import { initGallery } from './gallery-handler.js';
+import { printThumbnails} from './thumbnails-maker.js';
+import { initGallery } from './init-gallery.js';
 
 const thumbnailsData = generatePhotoData();
 
 printThumbnails(thumbnailsData);
-initGallery(thumbnailsBundle);
+initGallery(thumbnailsData);
 
