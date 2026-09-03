0ec3aca9c1044b0b1859466f64c520937944626e
Keks <keks@htmlacademy.ru>
2026-09-03 15:34:16 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/functions.js
@@ -0,0 +1,52 @@
+const checkLength = (line, length) => line.length <= length;
+
+checkLength('проверяемая строка', 20);
+
+
+const checkPalindrome = (line) => {
+  const normalizedLine = line.toLowerCase().replaceAll(' ', '');
+  let reversedLine = '';
+
+  for(let i = normalizedLine.length - 1; i >= 0; i--) {
+    reversedLine += normalizedLine[i];
+  }
+
+  return normalizedLine === reversedLine;
+};
+
+checkPalindrome('Лёша на полке клопа нашёл ');
+
+
+const retrieve = (line) => {
+  const lineString = line.toString();
+  let numberLine = '';
+
+  for(let i = 0; i < lineString.length; i++) {
+    if(!Number.isNaN(parseInt(lineString[i], 10))) {
+      numberLine += lineString[i];
+    }
+  }
+
+  return parseInt(numberLine, 10);
+};
+
+retrieve('1 кефир, 0.5 батона');
+
+
+const getMinutes = (time) => {
+  const [hours, minutes] = time.split(':');
+  return Number(hours) * 60 + Number(minutes);
+};
+
+// Функция проверяет что встреча не выходит за рамки рабочего дня
+const isMeetingValid = (start, end, meetStart, meetDuration) => {
+  const startTimeMinutes = getMinutes(start);
+  const endTimeMinutes = getMinutes(end);
+  const meetStartTimeMinutes = getMinutes(meetStart);
+  const endMeet = meetStartTimeMinutes + meetDuration;
+
+  // Встреча должна начинаться не раньше начала дня и заканчиваться не позже окончания дня
+  return meetStartTimeMinutes >= startTimeMinutes && endMeet <= endTimeMinutes;
+};
+
+isMeetingValid('08:00', '17:30', '17:00', 30);
